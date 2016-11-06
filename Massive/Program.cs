06959d5000f8eea33283dbc64bef690896e7294c
azhyla <anna.s.zhyla@gmail.com>
2016-11-06 20:20:47 +0200

    Massive1

---
+++ Massive/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Massive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numbers = new int[60];
+            Random r = new Random();
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Numbers[i] = r.Next(-500, 500);
+                Console.Write(Numbers[i] + " ");
+                            }
+            Console.Read();
+        }
+    }
+}
